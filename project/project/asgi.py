9f162f07c74c22a29fa453496f428f5687de5821
Harshit Sahu <harshitsahu.sh@gmail.com>
2024-10-10 16:55:18 +0530

    Added full functionality

--- project/project/asgi.py
+++ project/project/asgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+
+# imports
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from ChatApp import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    'http' : django_asgi_app,
+    'websocket' : URLRouter(
+        routing.websocket_urlpatterns           #on websocket protocol, see the url mapping from routings.py
+    )
+})
--- project/project/settings.py
+++ project/project/settings.py
@@ -44,6 +44,10 @@ INSTALLED_APPS = [
 
 ASGI_APPLICATION = 'project.asgi.application'
 
+CHANNEL_LAYERS = {
+    "default": {"BACKEND": "channels.layers.InMemoryChannelLayer"},
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
